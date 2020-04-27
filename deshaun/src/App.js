f18ae2272511766cb0ceb93327b1efa523e6364a
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-26 23:09:58 -0400

    added code to about page

--- deshaun/src/App.js
+++ deshaun/src/App.js
@@ -3,30 +3,31 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Nav';
 import HomeComponent from './pages/Home';
+import AboutPage from './pages/About';
 
 
 function App() {
   return (
     <div>
       <Router>
-      <div>
-      <Navbar/>
-      <br>
-      </br>
-      <div>
-             <Route exact path="/" component={HomeComponent} /> 
-            {/* <Route exact path="/about" component={AboutComponent} />
-            <Route exact path="/vitals" component={showVitals} />
+        <div>
+          <Navbar />
+          <br>
+          </br>
+          <div>
+            <Route exact path="/" component={HomeComponent} />
+            <Route exact path="/about" component={AboutPage} />
+            {/* <Route exact path="/vitals" component={showVitals} />
             <Route exact path="/family" component={showFamily} />
             <Route exact path="/patients" component={Patients} />
             <Route exact path="/nurse" component={LoginPage} />
             <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/signup" component={SignComponent} />
-            <Route exact path="/readygo" component={showMic} /> */}
+            <Route exact path="/signup" component={SignComponent} /> */}
+            {/* <Route exact path="/readygo" component={showMic} /> */}
              {/* <Route exact path="/patients/:id" component={Detail} /> */}
 
+          </div>
         </div>
-      </div>
       </Router>
     </div>
   );
--- deshaun/src/pages/About.js
+++ deshaun/src/pages/About.js
@@ -1,13 +1,22 @@
-import React from "react"; 
+import React from "react";
+import { MDBJumbotron, MDBContainer } from "mdbreact";
 
+const AboutPage = () => {
+    return (
+        <main>
+            <MDBJumbotron className="about-container" fluid>
+                <MDBContainer>
+                    <h2 className="display-4">Deshaun is the future of music </h2>
+                    <p className="lead"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                </MDBContainer>
+            </MDBJumbotron>
 
-
-const AboutPage=()=>{
-    return(
-        <div>
-            
-        </div>
-
+            <br>
+            </br>
+            <p className="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </main>
     )
 }
 
