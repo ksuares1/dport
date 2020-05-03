6340eaae8bff00028f13441d0b722e51bd32dadd
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-02 23:28:28 -0400

    update flute repairs page

--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -9,11 +9,9 @@ const FlutePage = () => {
           <MDBJumbotron style={{ padding: 0 }}>
             <MDBCol className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)` }}>
               <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Create your beautiful website with MDBootstrap</MDBCardTitle>
-                <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                  optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
-                </p>
-                <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
+                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Flute Repairs</MDBCardTitle>
+                <p className="mx-5 mb-5">Let's get your sound back in tune. Repairs can take 1-5 business days. Schedule a time to have your </p>
+                <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon>Get in Tune</MDBBtn>
               </MDBCol>
             </MDBCol>
           </MDBJumbotron>
