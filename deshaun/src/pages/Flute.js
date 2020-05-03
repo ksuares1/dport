f1e24a5b85a8c11412d0e86681a590afc809244a
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-02 22:19:28 -0400

    added code and imported into flute.js

--- deshaun/src/pages/Flute.js
+++ deshaun/src/pages/Flute.js
@@ -1,3 +1,12 @@
 import React from "react";
+import FlutePage from "../components/FluteR.js";
 
 
+const FluteRepairs= () =>{
+    return(
+        <FlutePage/>
+    )
+
+}
+
+export default FluteRepairs;
