76a22339a22faa86bd3afbe7a1daf8acec03b652
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-02 22:21:28 -0400

    added flute repairs to route

--- deshaun/src/App.js
+++ deshaun/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Nav';
 import HomeComponent from './pages/Home';
 // import AboutPage from './pages/About';
 import AboutComponent from './pages/About';
+import FluteRepairs from './pages/Flute';
 
 
 function App() {
@@ -16,12 +17,12 @@ function App() {
           <div>
             <Route exact path="/" component={HomeComponent} />
             <Route exact path="/about" component={AboutComponent} />
-            {/* <Route exact path="/vitals" component={showVitals} />
-            <Route exact path="/family" component={showFamily} />
+            <Route exact path="/flute" component={FluteRepairs} />
+            {/* <Route exact path="/family" component={showFamily} />
             <Route exact path="/patients" component={Patients} />
             <Route exact path="/nurse" component={LoginPage} />
             <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/signup" component={SignComponent} /> */}
+            <Route exact path="/signup" component={SignComponent} /> */} 
             {/* <Route exact path="/readygo" component={showMic} /> */}
              {/* <Route exact path="/patients/:id" component={Detail} /> */}
 
