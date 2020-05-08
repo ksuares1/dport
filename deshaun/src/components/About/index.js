850c7a58aec0d234cb9dd684083db3afb03924c0
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-07 22:30:41 -0400

    updated flute jumbotron

--- deshaun/src/components/About/index.js
+++ deshaun/src/components/About/index.js
@@ -7,6 +7,8 @@ const AboutPage = () => {
         <main className="text-align:center">
             <br>
             </br>
+            <br>
+            </br>
             <img className="about-pic" src={require("../../assets/img/deshaun-about.png")} alt=" Deshaun" />
             <p className="about-text" style={{ display: "inline-block", width:'50%', height:'50%'}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBIcon } from "mdbreact";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
-const FlutePage = () => {
+
+function FlutePage() {
   return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol>
-          <MDBJumbotron style={{ padding: 0 }}>
-            <MDBCol className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)` }}>
-              <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Flute Repairs</MDBCardTitle>
-                    <p>Let's get your flute back in tune.</p>
-                    <p>Schedule a repair consultation today.</p>
-                <p>Repairs can take 1-5 business days.</p>
-                <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon>Get in Tune</MDBBtn>
-              </MDBCol>
-            </MDBCol>
-          </MDBJumbotron>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Flute Repairs</h1>
+        <p>
+         Let me help you get in tune! Harmony is the key to your musicial successs. git 
+    </p>
+      </Container>
+    </Jumbotron>
   )
 }
 
