174639903b0cb1966e947db8f9b26b2facd848a1
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-08 15:59:59 -0400

    added background photo to jumbotron

--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { MDBJumbotron, MDBContainer } from "mdbreact";
-
+import flute from '../../assets/img/music-sheet.png';
   
     const FlutePage = () => {
       return (
         <MDBJumbotron className="flute-wrapper" fluid>
-          <MDBContainer>
+          <MDBContainer style={{backgroundImage: `url(${flute})`}}>
             <h2 className="display-4">Flute Repairs</h2>
             <p className="lead">Harmony is the key to your musicial successs. Let me help you get in tune!</p>
           </MDBContainer>
