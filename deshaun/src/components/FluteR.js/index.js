405a49499e15506ae71dc2dcc01cafc418aa1dde
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-08 17:33:09 -0400

    changed font size and color for flute page

--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { MDBJumbotron, MDBContainer } from "mdbreact";
 import flute from '../../assets/img/music-sheet.png';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+
   
     const FlutePage = () => {
       return (
-        <MDBJumbotron className="flute-wrapper" style={{backgroundImage: `url(${flute})`}}fluid>
-          <MDBContainer>
-            <h2 className="flute-head display-4">Flute Repairs</h2>
-            {/* <p className="flute-text lead">Harmony is the key to your musicial successs.</p>
-            <p className="flute-secline lead">Let me help you get in tune!</p> */}
-          </MDBContainer>
-        </MDBJumbotron>
+        <Jumbotron className="flute-wrapper" style={{backgroundImage: `url(${flute})`}} fluid>
+        <Container>
+          <h1 className="flute-head">Flute Repairs</h1>
+          {/* <p className="flute-text">
+          Harmony is the key to your musicial successs.
+          </p> */}
+          {/* Let me help you get in tune!</p> */} 
+        </Container>
+      </Jumbotron>
       )
     }
     
     export default FlutePage;
     
-    
+  
