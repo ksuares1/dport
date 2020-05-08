74ef5f88b2d643c158a3224b09da773a5d2c79e4
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-08 16:01:46 -0400

    adjusted background photo for flute page

--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -4,8 +4,8 @@ import flute from '../../assets/img/music-sheet.png';
   
     const FlutePage = () => {
       return (
-        <MDBJumbotron className="flute-wrapper" fluid>
-          <MDBContainer style={{backgroundImage: `url(${flute})`}}>
+        <MDBJumbotron className="flute-wrapper" style={{backgroundImage: `url(${flute})`}}fluid>
+          <MDBContainer>
             <h2 className="display-4">Flute Repairs</h2>
             <p className="lead">Harmony is the key to your musicial successs. Let me help you get in tune!</p>
           </MDBContainer>
