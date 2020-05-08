7a336398b05e06b16504fd13945724d3a5cf2a94
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-08 16:11:42 -0400

    removed some text from jumbotron flute

--- deshaun/src/components/FluteR.js/index.js
+++ deshaun/src/components/FluteR.js/index.js
@@ -6,8 +6,9 @@ import flute from '../../assets/img/music-sheet.png';
       return (
         <MDBJumbotron className="flute-wrapper" style={{backgroundImage: `url(${flute})`}}fluid>
           <MDBContainer>
-            <h2 className="display-4">Flute Repairs</h2>
-            <p className="lead">Harmony is the key to your musicial successs. Let me help you get in tune!</p>
+            <h2 className="flute-head display-4">Flute Repairs</h2>
+            {/* <p className="flute-text lead">Harmony is the key to your musicial successs.</p>
+            <p className="flute-secline lead">Let me help you get in tune!</p> */}
           </MDBContainer>
         </MDBJumbotron>
       )
