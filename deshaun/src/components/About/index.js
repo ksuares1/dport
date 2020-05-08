c7582c36be8e40e959a4a6f0af5e92d7cdc0b822
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-07 22:02:48 -0400

    about text is now displaying in line

--- deshaun/src/components/About/index.js
+++ deshaun/src/components/About/index.js
@@ -4,11 +4,12 @@ import React from "react";
 const AboutPage = () => {
     return (
         // row column issue
-        <main style={{flexDirection:"row"}}>
-            {/* <img className="about-pic" src={require("../../assets/img/deshaun-about.png")} alt=" Deshaun" /> */}
-            <p className="about-text">
+        <main>
+            <img className="about-pic" src={require("../../assets/img/deshaun-about.png")} alt=" Deshaun" />
+            <p className="about-text" style={{ display: "inline-block" }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
+        
         </main>
     )
 }
