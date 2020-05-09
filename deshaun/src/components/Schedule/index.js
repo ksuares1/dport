f02fac19370604b1837cefc0f655581fcca84d4f
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-08 20:24:52 -0400

    Added overlay to schedule

--- deshaun/src/components/Schedule/index.js
+++ deshaun/src/components/Schedule/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { MDBJumbotron, MDBContainer } from "mdbreact";
+import music from '../../assets/img/music-sheet.png';
 
 const SchedulePage = () => {
   return (
-    <MDBJumbotron className="schedule-wrapper" fluid>
-      <MDBContainer>
+    <MDBJumbotron className="schedule-wrapper"style={{ backgroundImage: `url(${music})` }} fluid>
+      <MDBContainer className="schedule" overlay="grey-light">
         <h2 className="display-4">Schedule a consultation today!</h2>
         <p className="lead"> Finding the right instructor is the key to your musical sucess!</p>
       </MDBContainer>
