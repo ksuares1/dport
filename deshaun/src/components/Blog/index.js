4db3e852a12370bb5ef7922b019b292fcd9dad65
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-26 22:17:57 -0400

    added css to blog page

--- deshaun/src/components/Blog/index.js
+++ deshaun/src/components/Blog/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./index.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 
 function BlogComponent() {
     return (
         <main>
-            <Jumbotron>
+            <br>
+            </br>
+            <Jumbotron  className="blog-jumbo" >
                 <h1>More than a Lyric </h1>
                 <p> A blog chronicling my musicial journey.</p>
                 <p>
-                    <Button variant="primary">Learn more</Button>
+                    <Button className="blog-btn" variant="default">Coming Soon</Button>
                 </p>
             </Jumbotron>
         </main>
